d70049e2cfecd6753f6bbf6bece90b0c588e1e86
jenjaina <jenni.kissinger@gmail.com>
2014-07-30 14:54:55 -0400

    Added ability to add and save new tasks

--- poker.js
+++ poker.js
@@ -8,7 +8,8 @@ app.controller("MainCtrl", function ($scope) {
     $scope.cardoptions = cards;
     $scope.name = "Guest";
     $scope.data = {
-        togglestart: true
+        togglestart: true,
+        tasknum: 0
     };
 
     $scope.addUser = function (newUser) {
@@ -17,10 +18,19 @@ app.controller("MainCtrl", function ($scope) {
         }
     }
 
+    $scope.addTask = function (newTask) {
+        if (angular.isDefined(newTask) && angular.isDefined(newTask.projectDescription)) {
+            $scope.cardchoices.push({ task: newTask.projectDescription, choices: [] }); 
+        } else {
+            $scope.cardchoices.push({ task: "", choices: [] });
+        }
+        $scope.data.tasknum += 1;
+    }
+
     $scope.addCard = function (newUser, newCard) {
         if (angular.isDefined(newUser) && angular.isDefined(newUser.name) &&
             angular.isDefined(newCard) && angular.isDefined(newCard.selectCard)) {
-            $scope.cardchoices.choices.push({
+            $scope.cardchoices[$scope.data.tasknum].choices.push({
                 user: newUser.name,
                 card: newCard.selectCard
             });
@@ -31,9 +41,9 @@ app.controller("MainCtrl", function ($scope) {
 
 var cards = [0, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100, "?", "coffee cup"];
 
-var model = {
+var model = [{
     task: "Writing a program",
     choices: [{ user: "Guest", card: "8" },
         { user: "Jenni", card: "100" }]
-};
+}];
 // })();
