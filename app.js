5e2c5da39436f2802bbcafda9870a35ab79feb36
jenjaina <jenni.kissinger@gmail.com>
2014-08-11 09:47:40 -0400

    Changed path

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express'),
 
 var port = process.env.PORT;
      
-app.use(express.static(__dirname)); // + '/public'));
+app.use(express.static(__dirname + '/site')); // + '/public'));
 
 io.sockets.on('connection', function(socket) {
     socket.on('createCard', function (data) {
