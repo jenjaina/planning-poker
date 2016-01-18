2acbc65bfba9912b824d7b86841dd2a79b469e5d
Jenni Kissinger <jkissinger@carekinesis.com>
2016-01-18 11:57:17 -0500

    Push

--- site/poker.js
+++ site/poker.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 // function () {
 
-var cards = [0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 34, 55, 89, 144, "?", "coffee cup"];
+var cards = [0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 34, 55, 89, 144, "??", "coffee cup"];
 
 var app = angular.module("PokerApp", ["socketService"]);
 
